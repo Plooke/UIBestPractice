8ffa96c741c036cf4d27bcf634e3ab523396f02a
liwei <liwei.ni@bytedance.com>
2020-08-21 10:46:33 +0800

    a exercise

---
+++ app/src/main/java/com/example/uibestpractice/DisplayUtil.kt
@@ -0,0 +1,14 @@
+package com.example.uibestpractice
+
+import android.content.Context
+import android.util.DisplayMetrics
+
+
+
+
+object DisplayUtil {
+    fun getScreenWidth(context: Context): Int {
+        val dm: DisplayMetrics = context.getResources().getDisplayMetrics()
+        return dm.widthPixels
+    }
+}
---
+++ app/src/main/java/com/example/uibestpractice/MainActivity.kt
@@ -0,0 +1,115 @@
+package com.example.uibestpractice
+
+import android.content.Context
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val msgList = ArrayList<Msg>()
+
+    private var adapter: MsgAdapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initMsg()
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        adapter = MsgAdapter(msgList)
+        recyclerView.adapter = adapter
+        recyclerView.scrollToPosition(adapter!!.itemCount-1)
+
+       /**
+       recyclerView.addOnLayoutChangeListener(View.OnLayoutChangeListener { v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom ->
+           if (bottom < oldBottom) {
+               recyclerView.post(Runnable {
+                   if (adapter!!.itemCount > 0) {
+                       recyclerView.smoothScrollToPosition(adapter!!.itemCount - 1)
+                   }
+               })
+           }
+       })
+       **/
+
+        send.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            send -> {
+                val content = inputText.text.toString()
+                if (content.isNotEmpty()) {
+                    val msg = Msg(content, Msg.TYPE_SENT)
+                    msgList.add(msg)
+                    adapter?.notifyItemInserted(msgList.size - 1)
+                    recyclerView.scrollToPosition(msgList.size - 1)
+                    inputText.setText("")
+                }
+            }
+        }
+    }
+
+    private fun initMsg() {
+        val msg1 = Msg("Hello guy.", Msg.TYPE_RECEIVED)
+        msgList.add(msg1)
+        val msg2 = Msg("hello. who is that?", Msg.TYPE_SENT)
+        msgList.add(msg2)
+        val msg3 = Msg("This is Tom. Nice talking to you. ", Msg.TYPE_RECEIVED)
+        msgList.add(msg3)
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (ev!!.getAction() === MotionEvent.ACTION_DOWN) {
+            val v = currentFocus
+            if (isShouldHideInput(v, ev!!)) {//点击的是其他区域，则调用系统方法隐藏软键盘
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                if (imm != null) {
+                    imm!!.hideSoftInputFromWindow(v!!.windowToken, 0)
+                }
+            }
+            return super.dispatchTouchEvent(ev)
+        }
+        // 必不可少，否则所有的组件都不会有TouchEvent了
+        return if (window.superDispatchTouchEvent(ev)) {
+            true
+        } else onTouchEvent(ev)
+    }
+
+    /**
+     * 判断是否是输入框区域
+     */
+    fun isShouldHideInput(v: View?, event: MotionEvent): Boolean {
+        if (v != null) {
+            when (v.id) {
+                R.id.inputText -> {
+                    val leftTop = intArrayOf(0, 0)
+                    //获取输入框当前的location位置
+                    v!!.getLocationInWindow(leftTop)
+                    val left = leftTop[0]
+                    val top = leftTop[1]
+                    val bottom = top + v!!.getHeight()
+                    val right = DisplayUtil.getScreenWidth(this)
+                    return if (event.x > left && event.x < right
+                        && event.y > top && event.y < bottom) {
+                        // 点击的是输入框区域，保留点击EditText的事件
+                        false
+                    } else {
+                        true
+                    }
+                }
+                else -> {
+                    return false
+                }
+            }
+        }
+        return false
+    }
+}
